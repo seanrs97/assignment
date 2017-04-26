10f72ca0e8e08c6411b0b8e037a7cd8949927966
sean stevenson <sean stevenson>
2017-04-26 17:39:27 +0100

    new stuff

--- serviceworker.js
+++ serviceworker.js
@@ -1,84 +1,91 @@
-var CACHE_NAME = 'gih-cache-v5';
+var BASE_PATH = '/assignment/';
+var CACHE_NAME = 'gih-cache-v6';
 var CACHED_URLS = [
   // Our HTML
-  'index.html',
-  'staffs-uni.html',
-  'sign-up.html',
-  'dino.png',
-  'mystyles.css',
-  'styles.css',
-  'offline.html',
-  'images/favicon/android-icon-36x36.png',
-  'images/favicon/android-icon-48x48.png',
-  'images/favicon/android-icon-72x72.png',
-  'images/favicon/android-icon-96x96.png',
-  'images/favicon/android-icon-144x144.png',
-  'images/favicon/android-icon-192x192.png',
-  'images/favicon/favicon-16x16.png',
-  'images/favicon/favicon-32x32.png',
-  'images/favicon/favicon-96x96.png',
-  'images/favicon/ic_launcher-1x.png',
-  'images/favicon/ic_launcher-2x.png',
-  'images/favicon/ic_launcher-3x.png',
-  'images/favicon/ic_launcher-4x.png',
-  'images/favicon/ic_launcher-5x.png',
-  'images/favicon/manifest.json',
-  'images/activities-image.jpg',
-  'images/banner.jpg',
-  'images/clubs-image.jpg',
-  'images/main-image.jpg',
-  'images/party-image.jpg',
-  'images/pic01.jpg',
-  'images/pic02.jpg',
-  'images/pic03.jpg',
-  'images/pic04.jpg',
-  'images/pic05.jpg',
-  'images/pic06.jpg',
-  'images/pic07.jpg',
-  'images/pic08.jpg',
-  'images/pic09.jpg',
-  'images/pic10.jpg',
-  'images/study-image.jpg',
-  'images/universityImage-1x.jpg',
-  'images/universityImage-2x.jpg',
-  'images/universityImage-3x.jpg',
-  'assets/css/images/overlay.png',
-  'assets/css/images/shadow.png',
-  'assets/css/font-awesome.min.css',
-  'assets/css/ie8.css',
-  'assets/css/main.css',
-  'assets/css/normalize.css',
-  'assets/fonts/FontAwesome.otf',
-  'assets/fonts/fontawesome-webfont.eot',
-  'assets/fonts/fontawesome-webfont.svg',
-  'assets/fonts/fontawesome-webfont.ttf',
-  'assets/fonts/fontawesome-webfont.woff',
-  'assets/fonts/fontawesome-webfont.woff2',
-  'assets/js/gen_validatorv31.js',
-  'assets/js/jquery.dropotron.min.js',
-  'assets/js/jquery.min.js',
-  'assets/js/main.js',
-  'assets/js/modernizr.js',
-  'assets/js/skel.min.js',
-  'assets/js/skel-viewport.min.js',
-  'assets/js/util.js',
-  'assets/js/ie/backgroundsize.min.htc',
-  'assets/js/ie/html5shiv.js',
-  'assets/js/ie/PIE.htc',
-  'assets/js/ie/respond.min.js',
-  'assets/sass/libs/_functions.scss',
-  'assets/sass/libs/_mixins.scss',
-  'assets/sass/libs/_skel.scss',
-  'assets/sass/libs/_vars.scss',
-  'assets/sass/ie8.scss',
-  'assets/sass/main.scss',
-  'assets/browserconfig.xml'
+  BASE_PATH + 'index.html',
+  BASE_PATH + 'staffs-uni.html',
+  BASE_PATH + 'sign-up.html',
+  BASE_PATH + 'dino.png',
+  BASE_PATH + 'mystyles.css',
+  BASE_PATH + 'styles.css',
+  BASE_PATH + 'offline.html',
+  BASE_PATH + 'images/favicon/android-icon-36x36.png',
+  BASE_PATH + 'images/favicon/android-icon-48x48.png',
+  BASE_PATH + 'images/favicon/android-icon-72x72.png',
+  BASE_PATH + 'images/favicon/android-icon-96x96.png',
+  BASE_PATH + 'images/favicon/android-icon-144x144.png',
+  BASE_PATH + 'images/favicon/android-icon-192x192.png',
+  BASE_PATH + 'images/favicon/favicon-16x16.png',
+  BASE_PATH + 'images/favicon/favicon-32x32.png',
+  BASE_PATH + 'images/favicon/favicon-96x96.png',
+  BASE_PATH + 'images/favicon/ic_launcher-1x.png',
+  BASE_PATH + 'images/favicon/ic_launcher-2x.png',
+  BASE_PATH + 'images/favicon/ic_launcher-3x.png',
+  BASE_PATH + 'images/favicon/ic_launcher-4x.png',
+  BASE_PATH + 'images/favicon/ic_launcher-5x.png',
+  BASE_PATH + 'images/favicon/manifest.json',
+  BASE_PATH + 'images/activities-image.jpg',
+  BASE_PATH + 'images/banner.jpg',
+  BASE_PATH + 'images/clubs-image.jpg',
+  BASE_PATH + 'images/main-image.jpg',
+  BASE_PATH + 'images/party-image.jpg',
+  BASE_PATH + 'images/pic01.jpg',
+  BASE_PATH + 'images/pic02.jpg',
+  BASE_PATH + 'images/pic03.jpg',
+  BASE_PATH + 'images/pic04.jpg',
+  BASE_PATH + 'images/pic05.jpg',
+  BASE_PATH + 'images/pic06.jpg',
+  BASE_PATH + 'images/pic07.jpg',
+  BASE_PATH + 'images/pic08.jpg',
+  BASE_PATH + 'images/pic09.jpg',
+  BASE_PATH + 'images/pic10.jpg',
+  BASE_PATH + 'images/study-image.jpg',
+  BASE_PATH + 'images/universityImage-1x.jpg',
+  BASE_PATH + 'images/universityImage-2x.jpg',
+  BASE_PATH + 'images/universityImage-3x.jpg',
+  BASE_PATH + 'assets/css/images/overlay.png',
+  BASE_PATH + 'assets/css/images/shadow.png',
+  BASE_PATH + 'assets/css/font-awesome.min.css',
+  BASE_PATH + 'assets/css/ie8.css',
+  BASE_PATH + 'assets/css/main.css',
+  BASE_PATH + 'assets/css/normalize.css',
+  BASE_PATH + 'assets/fonts/FontAwesome.otf',
+  BASE_PATH + 'assets/fonts/fontawesome-webfont.eot',
+  BASE_PATH + 'assets/fonts/fontawesome-webfont.svg',
+  BASE_PATH + 'assets/fonts/fontawesome-webfont.ttf',
+  BASE_PATH + 'assets/fonts/fontawesome-webfont.woff',
+  BASE_PATH + 'assets/fonts/fontawesome-webfont.woff2',
+  BASE_PATH + 'assets/js/gen_validatorv31.js',
+  BASE_PATH + 'assets/js/jquery.min.js',
+  BASE_PATH + 'assets/js/main.js',
+  BASE_PATH + 'assets/js/modernizr.js',
+  BASE_PATH + 'assets/js/skel.min.js',
+  BASE_PATH + 'assets/js/skel-viewport.min.js',
+  BASE_PATH + 'assets/js/util.js',
+  BASE_PATH + 'assets/js/ie/backgroundsize.min.htc',
+  BASE_PATH + 'assets/js/ie/html5shiv.js',
+  BASE_PATH + 'assets/js/ie/PIE.htc',
+  BASE_PATH + 'assets/js/ie/respond.min.js',
+  BASE_PATH + 'assets/sass/libs/_functions.scss',
+  BASE_PATH + 'assets/sass/libs/_mixins.scss',
+  BASE_PATH + 'assets/sass/libs/_skel.scss',
+  BASE_PATH + 'assets/sass/libs/_vars.scss',
+  BASE_PATH + 'assets/sass/ie8.scss',
+  BASE_PATH + 'assets/sass/main.scss',
+  BASE_PATH + 'assets/browserconfig.xml',
+  BASE_PATH + 'assets/js/offline-map.js'
+  BASE_PATH + 'assets/js/material.js',
+  BASE_PATH + 'event-default.png',
+  BASE_PATH + 'min-style.css',
+  BASE_PATH + 'offlinemap.jpg',
+  
   
   // Stylesheets and fonts
   // JavaScript
   // Images
 ];
 
+var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBMA8ic4_Gyh0D7XUW9gBtrCcEwoBbBJ8E&callback=initMap';
 self.addEventListener('install', function(event) {
   // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
   event.waitUntil(
@@ -88,28 +95,41 @@ self.addEventListener('install', function(event) {
   );
 });
 
+
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
-  if (requestURL.pathname === 'first.html') {
+  if (requestURL.pathname === 'staffs-uni.html') {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match('first.html').then(function(cachedResponse) {
-          var fetchPromise = fetch('first.html').then(function(networkResponse) {
-            cache.put('first.html', networkResponse.clone());
+        return cache.match('staffs-uni.html').then(function(cachedResponse) {
+          var fetchPromise = fetch('staffs-uni.html').then(function(networkResponse) {
+            cache.put('staffs-uni.html', networkResponse.clone());
             return networkResponse;
           });
           return cachedResponse || fetchPromise;
         });
       })
     );
+	
+	
+   } else if (requestURL.href === googleMapsAPIJS) {
+    event.respondWith(
+      fetch(
+        googleMapsAPIJS+'&'+Date.now(),
+        { mode: 'no-cors', cache: 'no-store' }
+      ).catch(function() {
+        return caches.match('offline-map.js');
+      })
+    );
   } else if (
     CACHED_URLS.includes(requestURL.href) ||
-    CACHED_URLS.includes(requestURL.pathname) {
+    CACHED_URLS.includes(requestURL.pathname)
+  ) {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match(event.request).then(function(response) {
           return response || fetch(event.request);
-        })
+        });
       })
     );
   }
@@ -121,7 +141,7 @@ self.addEventListener('activate', function(event) {
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (cacheName.startsWith('gih-cache-v5') && CACHE_NAME !== cacheName) {
+          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
             return caches.delete(cacheName);
           }
         })
@@ -129,3 +149,6 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+
+
+
